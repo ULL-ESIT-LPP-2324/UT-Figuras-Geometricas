4ca5c24b0920bf17ecb8d2710012b88c0daa2721
Coromoto León <cleon@ull.edu.es>
2023-10-11 11:17:16 +0000

    Pruebas - Cuadrado - Área

--- test/tc_figuras.rb
+++ test/tc_figuras.rb
@@ -11,11 +11,16 @@ class TestFiguras < Test::Unit::TestCase
   def test_simple_cuadrado
     #Integer
     assert_equal(16, perimetro_cuadrado(4))
+    assert_equal(16, area_cuadrado(4))
     #Float
     assert_equal(16.0, perimetro_cuadrado(4.0))
+    assert_equal(16.0, area_cuadrado(4.0))
   end
   def test_type_check
     assert_raise(ZeroDivisionError) {perimetro_cuadrado(1/0)}
+    assert_raise(ZeroDivisionError) {area_cuadrado(1/0)}
+    assert_raise(TypeError) {area_cuadrado("4")}
+    assert_raise(TypeError) {area_cuadrado('4')}
   end
   def test_failure
     assert_equal("4444", perimetro_cuadrado("4"))
